953551ac581791e18f5973e1d0e2101de740e455
Josen Romero <romerojosemanuel@hotmail.com>
2022-09-23 18:20:02 -0600

    fix test for fizzbuzz

--- other-exercises/fizzbuzz/fizzbuzz.test.js
+++ other-exercises/fizzbuzz/fizzbuzz.test.js
@@ -3,27 +3,23 @@ const fizzbuzz = require("./fizzbuzz");
 describe("fizzbuzz", () => {
 
     test("should print 1 if they receive 1", () => {
-        const expected = 1;
         const result = fizzbuzz(1);
-        expect(expected).toBe(result);
+        expect(result).toBe(1);
     });
-
+    
     test("should print fizz if they receive a multiple of 3", () => {
-        const expected = "fizz";
         const result = fizzbuzz(3);
-        expect(expected).toBe(result);
+        expect(result).toBe("fizz");
     });
 
     test("should print buzz if they receive a multiple of 5", () => {
-        const expected = "buzz";
         const result = fizzbuzz(5);
-        expect(expected).toBe(result);
+        expect(result).toBe("buzz");
     });
 
     test("should print fizzbuzz if they receive a multiple of 3 and 5", () => {
-        const expected = "fizzbuzz";
         const result = fizzbuzz(15);
-        expect(expected).toBe(result);
+        expect(result).toBe("fizzbuzz");
     });
 
 });
